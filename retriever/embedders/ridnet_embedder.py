951b21de872f1ef8f9edcd0ff053e6b86401bd03
Ricardo Carrillo <ricar60@mit.edu>
2025-08-26 17:54:13 -0400

    Added ridnet + face isolation with dlib

--- retriever/embedders/ridnet_embedder.py
+++ retriever/embedders/ridnet_embedder.py
@@ -2,6 +2,9 @@ import numpy as np
 import torch
 import torchvision.transforms as transforms
 from embedding.utils import extract_three_frames
+import dlib
+import cv2
+import numpy as np
 
 class Args:
     def __init__(self):
@@ -11,7 +14,7 @@ class Args:
 
 class RidnetEmbedder:
     def __init__(self):
-        self.model_name = "ridnet_pca_128"
+        self.model_name = "ridnet"
         self.mode = "video noise"
         self.model = self.load_model()  
 
@@ -45,11 +48,6 @@ class RidnetEmbedder:
                 emb = noise.flatten()
                 embeddings.append(emb)
 
-                # Not sure this is necessary here (outside of colab)
-                # # Free memory after each frame
-                # del keyframe, noise, emb
-                # gc.collect()
-
             except Exception as e:
                 print(f"Skipping frame due to error: {e}")
                 continue
@@ -57,8 +55,38 @@ class RidnetEmbedder:
         mean_emb = np.mean(embeddings, axis=0)
         return mean_emb
 
+    def isolate_face(img_array):
+        detector = dlib.get_frontal_face_detector()
+
+        # Load image
+        gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces
+        dlib_rects = detector(gray, 1)  # second arg = upsample times
+
+        if not dlib_rects:
+            print("No faces detected!")
+            return img_array
+
+        # Extract faces and ADD PADDING
+        for rect in dlib_rects:
+            x1 = rect.left()
+            y1 = rect.top()
+            x2 = rect.right()
+            y2 = rect.bottom()
+
+            # Crop and store face
+            face = img_array[y1:y2, x1:x2]
+
+            # Add padding to make it 224x224 if needed
+            top, bottom, left, right = 47, 48, 47, 48
+            padded = np.pad(face, ((top, bottom), (left, right), (0, 0)),
+           mode='constant', constant_values=0)  # -> (224,224,3)
+
+            return padded
+
     def get_video_face_noise(self, video: str) -> str:
-        pass  # Change this to get numpy arrays not strings
+        pass  # Change this to get numpy arrays not strings3
 
     # def embed(self, video: str, num_components: int = 128) -> np.ndarray:
     #     NOT SURE WE REALLY NEED THIS
