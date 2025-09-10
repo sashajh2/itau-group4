53d47c68e3a895973c5efad2576ffe9d289bde9b
Sasha Jovanovic-Hacon <ajovanov@mit.edu>
2025-09-10 13:18:51 -0400

    changing to use get client

--- dropbox/dropbox_utils.py
+++ dropbox/dropbox_utils.py
@@ -1,26 +1,38 @@
 #Wrapper around Dropbox SDK to abstract upload, download, and access-token logic. Helps avoid copy-pasting Dropbox boilerplate throughout your code.
 import dropbox as dropbox_sdk
+from dropbox.one_time_get_refresh_token import APP_KEY
 from utils.config_loader import load_config
 
-config = load_config()
+_config = load_config()
+_dbox_config = _config['dropbox']
 
-REFRESH_TOKEN = config['dropbox']['refresh_token']
+APP_KEY = _dbox_config['app_key']
+APP_SECRET = _dbox_config['app_secret']
+REFRESH_TOKEN = _dbox_config['refresh_token']
 
-dbx = dropbox_sdk.Dropbox(oauth2_refresh_token=REFRESH_TOKEN)
+_dbx = dropbox_sdk.Dropbox(
+    app_key=APP_KEY,
+    app_secret=APP_SECRET,
+    oauth2_refresh_token=REFRESH_TOKEN,
+    timeout=300
+)
+
+def get_client():
+    return _dbx
 
 def upload_file(local_path, dropbox_path, overwrite=True):
     mode = dropbox_sdk.files.WriteMode.overwrite if overwrite else dropbox_sdk.files.WriteMode.add
     with open(local_path, 'rb') as f:
         data = f.read()
     try:
-        dbx.files_upload(data, dropbox_path, mode=mode)
+        get_client().files_upload(data, dropbox_path, mode=mode)
         print(f"File uploaded successfully to {dropbox_path}")
     except dropbox_sdk.exceptions.ApiError as e:
         print(f"Error uploading file: {e}")
 
 def download_file(dropbox_path, local_path):
     try:
-        metadata = dbx.files_download_to_file(local_path, dropbox_path)
+        metadata = get_client().files_download_to_file(local_path, dropbox_path)
         print(f"File downloaded successfully to {local_path}")
         return metadata
     except dropbox_sdk.files.DownloadError as e:
@@ -29,7 +41,7 @@ def download_file(dropbox_path, local_path):
 
 def test_conection():
     try:
-        account = dbx.users_get_current_account()
+        account = get_client().users_get_current_account()
         print(f"Connected to Dropbox as {account.name.display_name}")
         return True
     except dropbox_sdk.exceptions.ApiError as e:
